afe9dee9f042e62c78c7e6dd6f4b084ee239e8ba
Abhishek Gupta <abhishek@Zorroh.local>
2016-06-10 01:42:46 +0530

    css of learn page

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ var states = [
         { name: 'base', state: { abstract: true, url: '', templateUrl: 'views/base.html', data: {text: "Base", visible: false } } },
         { name: 'login', state: { url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl', data: {text: "Login", visible: false } } },
         { name: 'main', state: { url: '/main', parent: 'base', templateUrl: 'views/main.html', controller: 'MainCtrl', data: {text: "Main", visible: false } } },
-        { name: 'learn', state: { url: '/learn', parent: 'main', templateUrl: 'views/main/learn.html', data: {text: "Learn", visible: true } } },
+        { name: 'learn', state: { url: '/learn', parent: 'main', templateUrl: 'views/main/learn.html', controller: 'LearnCtrl', data: {text: "Learn", visible: true } } },
         { name: 'wiki', state: { url: '/wiki', parent: 'main', templateUrl: 'views/main/wiki.html', data: {text: "Wiki", visible: true } } },
         { name: 'community', state: { url: '/community', parent: 'main', templateUrl: 'views/main/community.html', data: {text: "Community", visible: true } } },
         // { name: 'reports', state: { url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html', data: {text: "Reports", visible: true } } },
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -18,3 +18,58 @@ angular.module('yapp')
         }
     });
   });
+
+ angular.module('yapp')
+  .controller('LearnCtrl', ["$scope", function($scope) {
+  	$scope.greeting = "hello";
+ 	$scope.topBucket = [
+ 		{"name": "Agriculture",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 3,
+		 "media_total" : 9},
+		{"name": "Health & Hygiene",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 2,
+		 "media_total" : 7},
+		{"name": "Primary Education",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 4,
+		 "media_total" : 6},
+		{"name": "Secondary Education",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 3,
+		 "media_total" : 5},
+		{"name": "Competitive Exam Preparation",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 5,
+		 "media_total" : 5},
+		{"name": "Govt. Records and Local Governance Information",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 7,
+		 "media_total" : 9},
+		{"name": "Government Scheme Info",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 2,
+		 "media_total" : 4},
+		{"name": "Self Defence",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 5,
+		 "media_total" : 6},
+		{"name": "English for Job Skills",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 3,
+		 "media_total" : 3},
+		{"name": "Participatory Information by the Panchayat",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 7,
+		 "media_total" : 9},
+		{"name": "Vocational Education",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 3,
+		 "media_total" : 9},
+		{"name": "Skill training videos",
+		 "image":"images/agriculture.png",
+		 "media_consumed": 3,
+		 "media_total" : 8}
+ 	];
+ }]);
